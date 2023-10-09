fecb6b4ccf5e0beae3aa2b3b9617132c1d2efdcb
Liam McCausland <106557278+LiamMcCausland@users.noreply.github.com>
2023-10-08 23:03:17 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const container = document.querySelector(".container");
 
     // Load tasks from localStorage
-    const savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    let savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
     // Function to save tasks to localStorage
     function saveTasksToLocalStorage() {
@@ -15,11 +15,11 @@ document.addEventListener("DOMContentLoaded", function() {
     // Function to display tasks
     function displayTasks() {
         taskList.innerHTML = ""; // Clear the task list
-        savedTasks.forEach(function(taskText) {
+        savedTasks.forEach(function(taskData) {
             const li = document.createElement("li");
             li.innerHTML = `
-                <input type="checkbox">
-                <span>${taskText}</span>
+                <input type="checkbox" ${taskData.checked ? 'checked' : ''}>
+                <span>${taskData.text}</span>
                 <button class="delete-btn">Delete</button>
             `;
             taskList.appendChild(li);
@@ -32,7 +32,7 @@ document.addEventListener("DOMContentLoaded", function() {
     addTaskButton.addEventListener("click", function() {
         const taskText = taskInput.value.trim();
         if (taskText !== "") {
-            savedTasks.push(taskText);
+            savedTasks.push({ text: taskText, checked: false });
             saveTasksToLocalStorage();
             displayTasks();
             taskInput.value = "";
@@ -42,11 +42,18 @@ document.addEventListener("DOMContentLoaded", function() {
     taskList.addEventListener("click", function(event) {
         if (event.target.classList.contains("delete-btn")) {
             const taskToRemove = event.target.parentElement.querySelector("span").textContent;
-            const taskIndex = savedTasks.indexOf(taskToRemove);
+            const taskIndex = savedTasks.findIndex(task => task.text === taskToRemove);
             if (taskIndex !== -1) {
                 savedTasks.splice(taskIndex, 1);
                 saveTasksToLocalStorage();
                 displayTasks();
+            }
+        } else if (event.target.type === "checkbox") {
+            const taskText = event.target.nextElementSibling.textContent;
+            const taskIndex = savedTasks.findIndex(task => task.text === taskText);
+            if (taskIndex !== -1) {
+                savedTasks[taskIndex].checked = event.target.checked;
+                saveTasksToLocalStorage();
             }
         }
     });
